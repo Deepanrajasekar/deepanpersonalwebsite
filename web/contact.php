2b805cf5d341e304bf7b2d0fae0e5df86f993b6d
Deepanrajasekar <102378741+Deepanrajasekar@users.noreply.github.com>
2022-04-25 15:19:57 +0530

    Update contact.php

--- web/contact.php
+++ web/contact.php
@@ -1,10 +1,10 @@
 <?php
 // database connection code
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
-$host ="host=ec2-54-160-109-68.compute-1.amazonaws.com";
+$host ="ec2-3-229-252-6.compute-1.amazonaws.com";
 $port ="port=5432";
-$dbname ="dbname=d94vb9dvd4ste3";
-$credentials ="user=ydbnlatsdtsmte  password=7f3f7f6e8e94eaed6c1dc6d5cd538f7af3427fe24f762ecc7362a5c5d678735b";
+$dbname ="dbname=uwdosgsklyptut";
+$credentials ="uwdosgsklyptut  password=8ff71ced285a8df0fd4202fc69cff7d3f3a1cceef9bba14b5f1f1220492b8b96";
 $con = pg_connect( "$host $port $dbname $credentials");
 
 // get the post records
